a0170d9470c89a8fdde868d11197bd963afdb074
Talles Corradini <tallescorradini@gmail.com>
2022-05-04 10:16:54 -0300

    feat: add route to post feedbacks

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,44 @@
 import express from "express";
+import { prisma } from "./prisma";
+import nodemailer from "nodemailer";
 
 const app = express();
 
-app.listen(3333, () => {
-  console.log("Server running on port 3333");
+app.use(express.json());
+
+var transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: process.env.NODEMAILER_USER,
+    pass: process.env.NODEMAILER_PASS,
+  },
 });
 
-app.get("/users", (req, res) => {
-  return res.send("hello world");
+app.post("/feedbacks", async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    },
+  });
+
+  await transport.sendMail({
+    from: "Equipe Feeget <oi@feedget.com>",
+    to: "Talles Corradini <teste@gmail.com>",
+    subject: "Novo feedback",
+    html: [
+      `<p style="font-family: sans-serif; font-size: 16px; color: #111">Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+    ].join("\n"),
+  });
+
+  return res.status(201).json({ data: feedback });
+});
+
+app.listen(3333, () => {
+  console.log("Server running on port 3333");
 });
